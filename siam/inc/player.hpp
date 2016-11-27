9b485754ae47e3de6335bf1da652d24d757592ff
Romain <romainbrisse@live.fr>
2016-11-27 15:20:43 +0100

    enregistrement du choix du joueur pour l'animal à jouer

--- siam/inc/player.hpp
+++ siam/inc/player.hpp
@@ -3,18 +3,22 @@
 
 	#include <vector>
 	#include <string>
+	#include "../inc/object.hpp"
 
 	namespace Siam {
 
 		class Player {
 			private:
 				std::string m_name;
+				std::string m_animalchosen ;
 
 			public:
 				Player();
 				Player( std::string name );
 
 				~Player();
+
+				void savechoice(bool choice); //recevra le choix de l'user sous forme de vrai/faux
 		};
 
 	}
--- siam/player.cpp
+++ siam/player.cpp
@@ -1,18 +1,39 @@
 #include "inc/player.hpp"
+#include <iostream>
 
 using namespace std;
 using namespace Siam;
 
 Player::Player()
-		: m_name( "" ) {
+    : m_name( "" ) {
 
 }
 
 Player::Player( string name )
-		: m_name( name ) {
+    : m_name( name ) {
 
 }
 
 Player::~Player() {
+
+}
+
+void Player::savechoice(bool choice)
+{
+
+    if(choice == 0 || choice == 1)
+    {
+        if(choice == 0)
+        {
+            m_animalchosen = "Elephant" ;
+        }
+        if(choice == 1)
+        {
+            m_animalchosen = "Rhinoceros" ;
+        }
+
+    } else
+
+        cout << "Error : wrong value for choice" << endl ;
 
 }
