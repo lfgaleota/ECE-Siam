f16cb874e85f2fa54748e35b39f883a75b23ae50
Romain <romainbrisse@live.fr>
2016-11-29 09:07:28 +0100

    commentaires mountain.h et mountain.cpp

--- siam/objects/mountain.cpp
+++ siam/objects/mountain.cpp
@@ -5,16 +5,16 @@ using namespace std;
 using namespace Siam;
 using namespace Siam::Objects;
 
-Mountain::Mountain()
+Mountain::Mountain() //default constructor
 		: Object() {
 
 }
 
-Mountain::Mountain( string nom, float force )
+Mountain::Mountain( string nom, float force ) //overload constructor
 		: Object( nom, force ) {
 
 }
 
-Types::Type Mountain::getType() {
+Types::Type Mountain::getType() { //read access to type
 	return Types::Type::Mountain;
 }
