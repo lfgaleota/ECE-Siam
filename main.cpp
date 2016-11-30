e2b532cb8e340bf76bf444a1b76276cac384ad46
Romain <romainbrisse@live.fr>
2016-11-30 22:57:04 +0100

    Batterie de tests 1, commentaires dans le main à voir

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	std::vector<Siam::Player> players;
-	players.push_back( Siam::Player( "Louis-Félix", Siam::Objects::Types::Type::Rhinoceros ) );
+	players.push_back( Siam::Player( "Louis-Felix", Siam::Objects::Types::Type::Rhinoceros ) );
 	players.push_back( Siam::Player( "Romain", Siam::Objects::Types::Type::Elephant ) );
 
 
