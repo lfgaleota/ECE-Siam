979274308f73caaffef5e9ac8e2fc8870fac3cf6
Romain <romainbrisse@live.fr>
2016-12-03 19:01:04 +0100

    menu principal + ajout des sons.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include "siam/inc/game.hpp"
 #include "siam/inc/ui/games/gamecliui.hpp"
+#include "siam/inc/ui/audio/FMOD.hpp"
 
 void INeedPlayers ( std::vector<Siam::Player>& players)
 {
@@ -41,18 +42,67 @@ void newGame ()
    Sleep(2000);
 }
 
+void manageMusics(Siam::UI::Audio::FMOD& mediaPlayer)
+{
+
+    mediaPlayer.loadMusic("unity","C:/Users/romai/Desktop/Siam/Unity.wav");
+    mediaPlayer.playMusic("unity");
+    std::cout << "done" << std::endl ;
+
+}
+
 
 
 int main() {
-	std::vector<Siam::Player> players;
+    std::vector<Siam::Player> players;
+    Siam::UI::Audio::FMOD mediaPlayer ;
 
-	newGame();
-	INeedPlayers(players);
-	Siam::Game game = Siam::Game( players );
+    newGame();
+    manageMusics(mediaPlayer);
+    int choice=0, music=0 ;
 
 
 
-	return 0;
+    do
+    {
+        std::cout << "Entrez votre choix " << std::endl ;
+        std::cout << "1. Nouvelle Partie" << std::endl ;
+        std::cout << "2. Quitter le jeu" << std::endl ;
+        std::cout << "3. Sons" << std::endl ;
+        std::cin >> choice;
+
+        switch(choice)
+        {
+        case 1 :
+
+            INeedPlayers(players);
+            {
+                Siam::Game game = Siam::Game( players );
+            }
+            break ;
+
+        case 2 :
+
+            std::cout << "Merci d'avoir joue ! A bientot !" << std::endl ;
+            return 0;
+            break;
+
+        case 3 :
+
+            std::cout << "1. ON" << std::endl ;
+            std::cout << "2. OFF" << std::endl ;
+            std::cin >> music ;
+            if(music==1)
+                manageMusics(mediaPlayer);
+            else
+                mediaPlayer.stopMusic();
+
+
+            break ;
+
+
+        }
+    } while(choice !=0);
 }
 
 END_OF_MAIN();
