ba1b29cad0b0a1109052764829ca63d487b5790e
Romain <romainbrisse@live.fr>
2016-12-03 09:20:44 +0100

    ajout d'un logo et d'un sspg de demande des noms de joueurs. Peux tu centrer le logo et les affichages ? ce sera plus joli

--- main.cpp
+++ main.cpp
@@ -1,17 +1,56 @@
 #include "siam/inc/game.hpp"
 #include "siam/inc/ui/games/CLIbis.hpp"
+#include <fstream>
+
+void INeedPlayers ( std::vector<Siam::Player>& players)
+{
+    std::string nameOfPlayer ;
+
+    std::cout << "Entrez le nom du joueur 1 : " << std::endl ;
+    std::cin >> nameOfPlayer ;
+    players.push_back( Siam::Player( nameOfPlayer, Siam::Objects::Types::Type::Rhinoceros ) );
+
+    std::cout << "Entrez le nom du joueur 2 : " << std::endl ;
+    std::cin >> nameOfPlayer ;
+	players.push_back( Siam::Player( nameOfPlayer, Siam::Objects::Types::Type::Elephant ) );
+
+
+}
+
+void newGame ()
+{
+    std::ifstream logo("C:/Users/romai/Desktop/Siam/logoSiam.txt");
+
+   if(logo)
+   {
+     std::string ligne;
+
+      while(getline(logo, ligne))
+      {
+         std::cout << ligne << std::endl;
+      }
+   }
+   else
+   {
+      std::cout << "nope didn't work" << std::endl;
+   }
+
+   Sleep(2000);
+}
+
+
 
 int main() {
 	std::vector<Siam::Player> players;
-	players.push_back( Siam::Player( "Louis-Felix", Siam::Objects::Types::Type::Rhinoceros ) );
-	players.push_back( Siam::Player( "Romain", Siam::Objects::Types::Type::Elephant ) );
 
-	Siam::Game game = Siam::Game( players );
+	newGame();
+	INeedPlayers(players);
+    Siam::Game game = Siam::Game( players );
+
+
 
 	return 0;
 }
 
 ///ICI JE VAIS METTRE TOUS LES TRUCS QU'ILS FAUT CHANGER OU MODIFIER SUITE AUX TESTS
 
-///ON NE PEUT PAS REORIENTER ET BOUGER UNE PIECE EN MEME TEMPS,LE MOUVEMENT N'EST PAS ACCEPTE
-///LA VICTOIRE N'EST SANS DOUTE PAS APPELEE CAR IL N'Y A PAS D'ARRET DU JEU LORS DE LA SORTIE D'UNE MONTAGNE
