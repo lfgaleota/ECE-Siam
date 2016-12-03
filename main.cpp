ca0172bcc37a20ac9198937c85416bac8a6c2a87
Romain <romainbrisse@live.fr>
2016-12-03 19:02:11 +0100

    Revert "menu principal + ajout des sons."
    This reverts commit 5567458283ef0aa635937070bf70661ec15df4b1.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 
 #include "siam/inc/game.hpp"
 #include "siam/inc/ui/games/gamecliui.hpp"
-#include "siam/inc/ui/audio/FMOD.hpp"
 
 void INeedPlayers ( std::vector<Siam::Player>& players)
 {
@@ -42,67 +41,18 @@ void newGame ()
    Sleep(2000);
 }
 
-void manageMusics(Siam::UI::Audio::FMOD& mediaPlayer)
-{
-
-    mediaPlayer.loadMusic("unity","C:/Users/romai/Desktop/Siam/Unity.wav");
-    mediaPlayer.playMusic("unity");
-    std::cout << "done" << std::endl ;
-
-}
-
 
 
 int main() {
-    std::vector<Siam::Player> players;
-    Siam::UI::Audio::FMOD mediaPlayer ;
+	std::vector<Siam::Player> players;
 
-    newGame();
-    manageMusics(mediaPlayer);
-    int choice=0, music=0 ;
-
+	newGame();
+	INeedPlayers(players);
+	Siam::Game game = Siam::Game( players );
 
 
-    do
-    {
-        std::cout << "Entrez votre choix " << std::endl ;
-        std::cout << "1. Nouvelle Partie" << std::endl ;
-        std::cout << "2. Quitter le jeu" << std::endl ;
-        std::cout << "3. Sons" << std::endl ;
-        std::cin >> choice;
 
-        switch(choice)
-        {
-        case 1 :
-
-            INeedPlayers(players);
-            {
-                Siam::Game game = Siam::Game( players );
-            }
-            break ;
-
-        case 2 :
-
-            std::cout << "Merci d'avoir joue ! A bientot !" << std::endl ;
-            return 0;
-            break;
-
-        case 3 :
-
-            std::cout << "1. ON" << std::endl ;
-            std::cout << "2. OFF" << std::endl ;
-            std::cin >> music ;
-            if(music==1)
-                manageMusics(mediaPlayer);
-            else
-                mediaPlayer.stopMusic();
-
-
-            break ;
-
-
-        }
-    } while(choice !=0);
+	return 0;
 }
 
 END_OF_MAIN();
