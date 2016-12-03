7419d0f5a5ffd87e6593ad87701af185246347e5
louisfelix90 <louisfelix@galeota.fr>
2016-12-03 18:24:00 +0100

    Mise à jour de l'interface Allegro

--- siam/inc/ui/games/gameallegroui.hpp
+++ siam/inc/ui/games/gameallegroui.hpp
@@ -1,13 +1,66 @@
 #ifndef _ECE_SIAM_UI_GAMES_ALLEGRO_HPP_
 	#define _ECE_SIAM_UI_GAMES_ALLEGRO_HPP_
 
+	#include <allegro.h>
+	#include "../../../../winhack.h"
+	#include <jpgalleg.h>
+	#include <loadpng.h>
+	#include <vector>
+	#include <ios>
+	#include "../../../../allegro/popup.hpp"
 	#include "../gameui.hpp"
+	#include "../audio/FMOD.hpp"
 
 	namespace Siam {
 		namespace UI {
 			namespace Games {
-				class Allegro : public Siam::UI::Game {
+				class AllegroObject {
+					private:
+						unsigned int m_x;
+						unsigned int m_y;
+						Siam::Objects::Types::Type m_type;
+						Siam::Matrixs::Direction m_direction;
 
+					public:
+						AllegroObject();
+						AllegroObject( unsigned int x, unsigned int y, Siam::Objects::Types::Type type, Siam::Matrixs::Direction direction );
+
+						unsigned int getX() const;
+						void setX( unsigned int x );
+
+						unsigned int getY() const;
+						void setY( unsigned int y );
+
+						Matrixs::Direction getDirection() const;
+						void setDirection( Matrixs::Direction direction );
+
+				};
+
+				class Allegro : public Siam::UI::Game {
+					private:
+						std::unordered_map<const Siam::Object*, AllegroObject> m_objects;
+						std::unordered_map<std::string, BITMAP*> m_bitmaps;
+						Siam::UI::Audio::FMOD fmod;
+						BITMAP* page;
+
+						void loadSprites();
+
+						void display();
+
+					public:
+						Allegro( const std::vector<std::vector<Siam::Object*>>& board, const std::vector<Siam::Player>& players, std::vector<Siam::Player>::iterator& currentPlayer );
+						~Allegro();
+
+						void showError( std::string msg );
+						void getPlayerCoords( unsigned int& x, unsigned int& y, Siam::Matrixs::Direction* dir );
+						void addPiece( const Object*, unsigned int x, unsigned int y );
+						void removePiece( const Object* );
+						void movePiece( std::map<const Siam::Object*, std::pair<unsigned int, unsigned int>>& movements );
+						void orientPiece( const Object* );
+						void playerTurnBegin( Siam::Player& player );
+						void playerTurnEnd( Siam::Player& player );
+						Players::Action getPlayerAction( Siam::Player& player );
+						void victory( Siam::Player& player );
 				};
 			}
 		}
--- siam/ui/games/gameallegroui.cpp
+++ siam/ui/games/gameallegroui.cpp
@@ -1,1 +1,159 @@
 #include "../../inc/ui/games/gameallegroui.hpp"
+
+using namespace std;
+using namespace Siam;
+using namespace Siam::Objects;
+using namespace Siam::Matrixs;
+using namespace Siam::UI::Games;
+
+AllegroObject::AllegroObject() : m_x( 0 ), m_y( 0 ), m_type( Types::Type::Object ), m_direction( Direction::Left ) {}
+
+AllegroObject::AllegroObject( unsigned int x, unsigned int y, Types::Type type, Direction direction ) : m_x( x ), m_y( y ), m_type( type ), m_direction( direction ) {}
+
+unsigned int AllegroObject::getX() const {
+	return this->m_x;
+}
+
+void AllegroObject::setX( unsigned int x ) {
+	this->m_x = x;
+}
+
+unsigned int AllegroObject::getY() const {
+	return this->m_y;
+}
+
+void AllegroObject::setY( unsigned int y ) {
+	this->m_y = y;
+}
+
+Direction AllegroObject::getDirection() const {
+	return m_direction;
+}
+
+void AllegroObject::setDirection( Direction direction ) {
+	this->m_direction = direction;
+}
+
+Allegro::Allegro( const vector<vector<Object*>>& board, const vector<Player>& players, vector<Player>::iterator& currentPlayer ) : Game( board, players, currentPlayer ) {
+	allegro_init();
+	install_mouse();
+	install_keyboard();
+
+	jpgalleg_init();
+	register_png_file_type();
+	install_keyboard();
+	install_mouse();
+
+	set_color_depth( desktop_color_depth() );
+	if( set_gfx_mode( GFX_AUTODETECT_WINDOWED, 1280, 720, 0, 0 ) != 0 ) {
+		allegro_message( "probleme mode graphique" );
+		allegro_exit();
+		throw runtime_error( "Allegro initialization failed" );
+	}
+
+	set_window_title( "ECE-Siam" );
+
+	enable_hardware_cursor();
+	select_mouse_cursor( 2 );
+	show_mouse( screen );
+
+	loadSprites();
+	page = create_bitmap( SCREEN_W, SCREEN_H );
+	display();
+}
+
+Allegro::~Allegro() {
+	for( auto& bmp : this->m_bitmaps ) {
+		destroy_bitmap( bmp.second );
+	}
+}
+
+void Allegro::loadSprites() {
+	string path;
+	BITMAP* bmp;
+	vector<string> bmpNames = {
+			"rhinoceros",
+			"elephant",
+	        "mountain",
+	        "case"
+	};
+
+	for( const auto& bmpName : bmpNames ) {
+		path = "images/";
+		path += bmpName;
+		path += ".png";
+
+		bmp = load_bitmap( path.c_str(), 0 );
+		if( bmp != nullptr ) {
+			this->m_bitmaps.insert( make_pair( bmpName, bmp ) );
+		} else {
+			path = "File not found: ";
+			path = "images/";
+			path += bmpName;
+			path += ".png";
+			throw ios_base::failure( path );
+		}
+	}
+}
+
+void Allegro::showError( string msg ) {
+	Functions::Allegro::Popup( "Erreur", msg );
+}
+
+void Allegro::display() {
+	clear_bitmap( page );
+
+	blit( page, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H );
+}
+
+
+void Allegro::getPlayerCoords( unsigned int& x, unsigned int& y, Matrixs::Direction* dir ) {
+
+}
+
+void Allegro::addPiece( const Object* object, unsigned int x, unsigned int y ) {
+	if( object != nullptr )
+		this->m_objects.insert( make_pair( object, AllegroObject( x, y, object->getType(), object->getDirection() ) ) );
+
+	display();
+}
+
+void Allegro::removePiece( const Object* object ) {
+	this->m_objects.erase( object );
+
+	display();
+}
+
+void Allegro::movePiece( map<const Object*, pair<unsigned int, unsigned int>>& movements ) {
+	for( const auto& movement : movements ) {
+		if( movement.first != nullptr ) {
+			this->m_objects[ movement.first ].setX( movement.second.first );
+			this->m_objects[ movement.first ].setY( movement.second.second );
+		}
+	}
+
+	display();
+}
+
+void Allegro::orientPiece( const Object* object ) {
+	if( object != nullptr )
+		this->m_objects[ object ].setDirection( object->getDirection() );
+
+	display();
+}
+
+void Allegro::playerTurnBegin( Player& player ) {
+	display();
+}
+
+void Allegro::playerTurnEnd( Player& player ) {
+	display();
+}
+
+Players::Action Allegro::getPlayerAction( Player& player ) {
+	return Players::Action::Nothing;
+}
+
+void Allegro::victory( Player& player ) {
+
+}
