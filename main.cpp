59518371aba6dc79e9409e222a5bf5c36da91998
louisfelix90 <louisfelix@galeota.fr>
2016-12-04 23:09:50 +0100

    Possibilité de switcher entre les interfaces

--- main.cpp
+++ main.cpp
@@ -6,19 +6,24 @@
 #include "siam/inc/ui/main/maincliui.hpp"
 #include "siam/inc/ui/main/mainallegroui.hpp"
 
-
-int main() {
+int main( int argc, char* argv[] ) {
 	Siam::UI::Audio::FMOD fmod;
-
 
 	fmod.loadMusic( "menu", "musics/menu.it" );
 	fmod.loadMusic( "main1", "musics/main1.it" );
 	fmod.loadMusic( "victory", "musics/victorybg.it" );
 	fmod.loadSound( "victory", "musics/victory.it" );
 
-	Siam::UI::Main::CLI ui( fmod );
+	if( argc > 0 ) {
+		std::string arg0 = argv[ 0 ];
 
+		if( arg0 == "--cli" ) {
+			Siam::UI::Main::CLI ui( fmod );
+			return 0;
+		}
+	}
 
+	Siam::UI::Main::Allegro ui( fmod );
 	return 0;
 }
 
